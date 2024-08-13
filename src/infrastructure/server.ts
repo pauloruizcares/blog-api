a8471835b0cbc0db28807e97a8e3510bcf3ff03c
Paulo Ruiz <paulo.ruiz@mercadolibre.cl>
2024-08-12 22:52:24 -0400

    :rotating_light: fix: change default port

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -22,7 +22,7 @@ const corsOptions = {
 };
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cookieParser());
 app.use(cors(corsOptions));
